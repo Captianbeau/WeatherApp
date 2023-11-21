48aae47d59a5b57c04681af35fe0421cf8919dd6
Capti <captianbeau@gmail.com>
2023-11-21 15:17:33 -0700

    added comments

---
+++ script.js
@@ -0,0 +1,4 @@
+// TODO fetch weather data, sort it by:temp, conditions, cloud coverage, location. 
+// TODO use inputs to get the data for specific cities  maybe(use the city data to make an auto-correct)
+// TODO eventListeners for buttons and add buttons for previous cities
+// TODO local storage for previously searched cities
