321f6ba414ebd41bfadfd0886071ce230c4df81a
Capti <captianbeau@gmail.com>
2024-03-07 14:27:13 -0700

    added fetch url and api key to js.

--- script.js
+++ script.js
@@ -2,8 +2,14 @@
 // TODO use inputs to get the data for specific cities  maybe(use the city data to make an auto-correct)
 // TODO eventListeners for buttons and add buttons for previous cities
 // TODO local storage for previously searched cities
-const requestUrl = '';
+const APIKey = "f83cd6c573bb9467dfb73fbb0a6f5d02";
+
+let city;
+
+const requestUrl = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${APIKey}`;
+
+
 fetch(requestUrl)
     .then(function (response){
-        return response,json();
+        return response.json();
     })
