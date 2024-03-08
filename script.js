bfc9bd592fe37f853117f95f37be265ff0a491f7
Capti <captianbeau@gmail.com>
2024-03-08 16:11:02 -0700

    tested call

--- script.js
+++ script.js
@@ -2,10 +2,12 @@
 // TODO use inputs to get the data for specific cities  maybe(use the city data to make an auto-correct)
 // TODO eventListeners for buttons and add buttons for previous cities
 // TODO local storage for previously searched cities
+
+// needs lat and lon to get good location
 const APIKey = "f83cd6c573bb9467dfb73fbb0a6f5d02";
 
 let city;
-
+city = 'Ogden';
 const requestUrl = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${APIKey}`;
 
 
@@ -13,3 +15,7 @@ fetch(requestUrl)
     .then(function (response){
         return response.json();
     })
+    .then(function(data){
+        
+        console.log(data)
+    });
