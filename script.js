4314b0ed6d20efd19a0d8fed6d42cfe99074d50f
Capti <captianbeau@gmail.com>
2024-03-13 16:25:41 -0600

    added classes to created elements and added some styling to them

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 // TODO use inputs to get the data for specific cities  maybe(use the city data to make an auto-correct)
 // TODO eventListeners for buttons and add buttons for previous cities
 // TODO local storage for previously searched cities
-const forecastArea = document.querySelector('#forecast')
+const forecastArea = document.querySelector('.dailyweather')
 // needs lat and lon to get good location
 const searchBtn= document.querySelector('#searchBtn');
 const APIKey = "f83cd6c573bb9467dfb73fbb0a6f5d02";
@@ -18,21 +18,24 @@ function printForecast(resultData){
     
 
     
-if(dayjs(resultData.dt_txt).format('h') === '9'){
+if(dayjs(resultData.dt_txt).format('h') === '12'){
     console.log(resultData)
 
 if(resultData.sys.pod === 'n'){
-    const date = document.createElement('h4');
-    date.textContent = dayjs(resultData.dt_txt).format('ddd')+ " ";
+    // const date = document.createElement('h4');
+    // date.textContent = dayjs(resultData.dt_txt).format('ddd D')+ " Night";
     const tempNight = document.createElement('h3');
     tempNight.textContent = resultData.main.temp_min + 'F Low';
 
     const conditionsNight = document.createElement('p');
     conditionsNight.textContent = 'Night conditions: '+ resultData.weather[0].main +', '+ resultData.weather[0].description;
-    tempDiv.append(date, tempNight)
+    forecastDay.classList.add('night')
+    tempDiv.append( tempNight)
     forecastBody.append(conditionsNight);
 }else{
 
+    const date = document.createElement('h4');
+    date.textContent = dayjs(resultData.dt_txt).format('ddd D')+ " ";
 
     const temp = document.createElement('h3');
     temp.textContent = resultData.main.temp + 'F High ';
@@ -42,7 +45,8 @@ if(resultData.sys.pod === 'n'){
 
     const cloudCoverage = document.createElement('p');
     cloudCoverage.textContent = resultData.weather[0].description + ' '+ resultData.clouds.all+'% coverage';
-    tempDiv.appendChild(temp)
+    forecastDay.classList.add('day')
+    tempDiv.append(date,temp)
     forecastBody.append(conditions,cloudCoverage);
 }
 
